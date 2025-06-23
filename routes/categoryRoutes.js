c6204b79b680c526b467118dd02fa0a24da638eb
semihkececioglu <semihkecec@gmail.com>
2025-06-23 12:54:33 +0300

    some updated

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Category = require(".../models/Category");
+const Category = require("../models/Category");
 
 // Get all categories
 router.get("/", async (req, res) => {
