910c9a5b2476816ac2a6e8b8050c756b44fa68f0
semihkececioglu <semihkecec@gmail.com>
2025-06-24 14:56:45 +0300

    image upload problem fix cloudinary

--- routes/upload.js
+++ routes/upload.js
@@ -1,28 +1,18 @@
 const express = require("express");
 const multer = require("multer");
-const path = require("path");
+const { storage } = require("../utils/cloudinary");
 
 const router = express.Router();
-
-// Yükleme klasörü ve dosya ismi ayarı
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    const uniqueName = Date.now() + "-" + file.originalname;
-    cb(null, uniqueName);
-  },
-});
-
 const upload = multer({ storage });
 
-// Yükleme işlemi
+// ✅ Cloudinary'e yükleme
 router.post("/", upload.single("image"), (req, res) => {
-  if (!req.file) return res.status(400).json({ error: "Dosya yüklenemedi" });
+  if (!req.file || !req.file.path) {
+    return res.status(400).json({ error: "Görsel yüklenemedi" });
+  }
 
-  const imageUrl = `https://materialblog-server-production.up.railway.app/uploads/${req.file.filename}`;
-  res.json({ url: imageUrl }); // ✅ Doğrudan tam URL döndür
+  // Cloudinary'den dönen url
+  res.json({ url: req.file.path });
 });
 
 module.exports = router;
