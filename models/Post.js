6083b4b824e478c6251dbd8a0b2502ccd7fed8d5
semihkececioglu <semihkecec@gmail.com>
2025-06-25 15:42:01 +0300

    slug fix

--- models/Post.js
+++ models/Post.js
@@ -3,6 +3,7 @@ const slugify = require("slugify");
 
 const postSchema = new mongoose.Schema({
   title: String,
+  slug: { type: String, unique: true },
   summary: String,
   content: String,
   category: String,
@@ -18,10 +19,12 @@ const postSchema = new mongoose.Schema({
 
 // Her post kaydedilmeden önce categorySlug otomatik üret
 postSchema.pre("save", function (next) {
+  if (this.title && !this.slug) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
   if (this.category) {
     this.categorySlug = slugify(this.category, { lower: true, strict: true });
   }
   next();
 });
-
 module.exports = mongoose.model("Post", postSchema);
